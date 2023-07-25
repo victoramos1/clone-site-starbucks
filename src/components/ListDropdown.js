9854fd7e38a7e2f9c9757135a37ec5b064d55f18
victoramos1 <ramos_victor@outlook.com>
2023-07-25 18:42:50 -0300

    Finalização de responsividade

--- src/components/ListDropdown.js
+++ src/components/ListDropdown.js
@@ -1,9 +1,59 @@
 import imgHistoriasLatinas from '../img/img-historias-latinas.svg'
 import './ListDropdown.css'
+import { useState } from "react"
 
-export default function ListDropdown(){
+export default function ListDropdown() {
 
-    return(
+    const itens = [
+        {
+            titulo: 'Sobre nós',
+            conteudo: <ul>
+                <li>Nossa empresa</li>
+                <li>Nosso café</li>
+                <li>Atendimento ao cliente</li>
+                <li>Compliance e privacidade</li>
+                <li>Código de Ética e Conduta SouthRock</li>
+            </ul>
+        },
+        {
+            titulo: 'Carreira',
+            conteudo: <ul>
+                <li>Central de carreira</li>
+            </ul>
+        },
+        {
+            titulo: 'Impacto Social',
+            conteudo: <ul>
+                <li>Comunidade</li>
+                <li>Meio Ambiente</li>
+                <li>Fornecimento ético</li>
+                <li>Histórias Starbucks</li>
+            </ul>
+        },
+        {
+            titulo: 'Starbucks Rewards',
+            conteudo: <ul>
+                <li>Baixe o aplicativo</li>
+                <li>Termos & Condições do Starbucks Card</li>
+                <li>Termos & Condições do<br /> Starbucks Rewards</li>
+                <li>Termos & Condições do<br /> Gift Card</li>
+                <li>Termos & Condições de<br /> Promoções Starbucks</li>
+                <li>Termos & Condições Peça<br /> e Pague pelo Celular e Retire na Loja</li>
+            </ul>
+        }
+    ]
+
+    const [selecionado, setSelecionado] = useState(null)
+
+    function cliqueBotao(index) {
+        if (selecionado === index) {
+            return setSelecionado(null)
+        } else {
+            setSelecionado(index)
+        }
+    }
+
+    return (
         <div>
             <div className='rodape'>
                 <div className='formatacao_rodape'>
@@ -12,7 +62,7 @@ export default function ListDropdown(){
                     <p>Nosso café</p>
                     <p>Atendimento ao cliente</p>
                     <p>Compliance e privacidade</p>
-                    <p>Código de Ética e Conduta <br/>SouthRock</p>
+                    <p>Código de Ética e Conduta <br />SouthRock</p>
                 </div>
                 <div className='formatacao_rodape'>
                     <h3>Carreira</h3>
@@ -28,54 +78,29 @@ export default function ListDropdown(){
                 <div className='formatacao_rodape'>
                     <h3>Starbucks Rewards</h3>
                     <p>Baixe o aplicativo</p>
-                    <p>Termos & Condições do<br/> Starbucks Card</p>
-                    <p>Termos & Condições do<br/> Starbucks Rewards</p>
-                    <p>Termos & Condições do<br/> Gift Card</p>
-                    <p>Termos & Condições de<br/> Promoções Starbucks</p>
-                    <p>Termos & Condições Peça<br/> e Pague pelo Celular e Retire<br/> na Loja</p>
+                    <p>Termos & Condições do<br /> Starbucks Card</p>
+                    <p>Termos & Condições do<br /> Starbucks Rewards</p>
+                    <p>Termos & Condições do<br /> Gift Card</p>
+                    <p>Termos & Condições de<br /> Promoções Starbucks</p>
+                    <p>Termos & Condições Peça<br /> e Pague pelo Celular e Retire<br /> na Loja</p>
                 </div>
-                <img src={imgHistoriasLatinas} className='historias_latinas' alt="Logo Histórias Latinas"/>
+                <img src={imgHistoriasLatinas} className='historias_latinas' alt="Logo Histórias Latinas" />
             </div>
             {/*Versão mobile*/}
             <div className="rodape_mobile">
                 <section>
-                    <details>
-                        <summary>Sobre nós</summary>
-                        <ul>
-                            <li>Nossa empresa</li>
-                            <li>Nosso café</li>
-                            <li>Atendimento ao cliente</li>
-                            <li>Compliance e privacidade</li>
-                            <li>Código de Ética e Conduta SouthRock</li>
-                        </ul>
-                    </details>
-                    <details>
-                        <summary>Carreira</summary>
-                            <ul>
-                                <li>Central de carreira</li>
-                            </ul>
-                    </details>
-                    <details>
-                        <summary>Impacto Social</summary>
-                        <ul>
-                            <li>Comunidade</li>
-                            <li>Meio Ambiente</li>
-                            <li>Fornecimento ético</li>
-                            <li>Histórias Starbucks</li>
-                        </ul>
-                    </details>
-                    <details>
-                        <summary>Starbucks Rewards</summary>
-                        <ul>
-                            <li>Baixe o aplicativo</li>
-                            <li>Termos & Condições do Starbucks Card</li>
-                            <li>Termos & Condições do<br/> Starbucks Rewards</li>
-                            <li>Termos & Condições do<br/> Gift Card</li>
-                            <li>Termos & Condições de<br/> Promoções Starbucks</li>
-                            <li>Termos & Condições Peça<br/> e Pague pelo Celular e Retire na Loja</li>
-                        </ul>
-                    </details>
-                    <img src={imgHistoriasLatinas} className='historias_latinas' alt="Logo Histórias Latinas"/>
+                    {itens.map((item, index) =>
+                        <div className='main-container' key={index}>
+                            <div className='container-titulo'>
+                                <h3>{item.titulo}</h3>
+                                <button onClick={() => cliqueBotao(index)}>{selecionado === index ? "˄" : '˅'}</button>
+                            </div>
+                            <div className={selecionado === index ? 'container-visivel' : 'container-escondido'}>
+                                <p>{item.conteudo}</p>
+                            </div>
+                        </div>
+                    )}
+                    <img src={imgHistoriasLatinas} className='historias_latinas' alt="Logo Histórias Latinas" />
                 </section>
             </div>
         </div>
